d18b962bf717d297fa154159b21f1ca9a9946861
Madhumitha <madhumithabiw@gmail.com>
2018-10-24 11:23:37 -0500

    Binary Search

--- Chapter3/Code/Exercise_3.1.c
+++ Chapter3/Code/Exercise_3.1.c
@@ -1,1 +1,23 @@
+int BinarySearch(int x, int v[]. int n)
+{
+  int low,high, mid;
+  low = 0;
+  high = n-1;
+  mid=(high+low)/2;
+  
+  while(low <= high && x != v[mid])
+  {
+    
+    if(x < v[mid])
+      high = mid-1;
+    else (x>v[mid])
+      low=mid+1;
+    mid = (low+high)/2;
+  }
+  if(x==v[mid])
+    return mid;
+  else
+    return -1;
+}
 
+    
